afd14a873226a455bab8cdb005d89ae8d6f57437
SarahAlnasser <sarahalnasser04@gmail.com>
2025-10-25 23:45:17 +0300

    Initial Commit

---
+++ plants🍀/plants__App.swift
@@ -0,0 +1,17 @@
+//
+//  plants__App.swift
+//  plants🍀
+//
+//  Created by Sarah Alnasser on 25/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct plants__App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
